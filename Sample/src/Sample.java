db80bd8f6501c394c4ea58d16d36e851a9162550
goodee <goodee@DESKTOP-OBOOCN7>
2018-11-01 16:04:25 +0900

    아 진짜 먼디

--- Sample/src/Sample.java
+++ Sample/src/Sample.java
@@ -3,7 +3,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//팟 아멀라
+		//팟 먼디
 		System.out.println("hello");
 		System.out.println("hello");
 		
