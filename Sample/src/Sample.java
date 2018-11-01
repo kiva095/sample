3588f6452bdcf2252c368f880e7e3358b49dae9c
goodee <goodee@DESKTOP-OBOOCN7>
2018-11-01 16:19:54 +0900

    씁

--- Sample/src/Sample.java
+++ Sample/src/Sample.java
@@ -4,11 +4,9 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//팟 ㅇㅇ
-		System.out.println("hello");
-		System.out.println("hello");
+		System.out.println("Hello KIVA?");
+		System.out.println("Hello!!");
 		System.out.println("하!하!");
-		
-		//da
 	
 
 	}
