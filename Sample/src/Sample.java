9d7dacdf382d9a28fc2416b42e9bc2e38bf2da6f
goodee <goodee@DESKTOP-OBOOCN7>
2018-11-01 16:03:36 +0900

    멀라

--- Sample/src/Sample.java
+++ Sample/src/Sample.java
@@ -3,7 +3,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//팟 ㅇㅇ
+		//팟 아멀라
 		System.out.println("hello");
 		System.out.println("hello");
 		
